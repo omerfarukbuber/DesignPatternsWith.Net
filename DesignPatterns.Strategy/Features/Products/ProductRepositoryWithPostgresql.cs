484ef6ad0f3ed77c84d236bcee40a6077f60d5fe
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-11-20 23:28:34 +0300

    Create postgre and mongo implementations of IProductRepository

---
+++ DesignPatterns.Strategy/Features/Products/ProductRepositoryWithPostgresql.cs
@@ -0,0 +1,81 @@
+﻿using DesignPatterns.Strategy.Database;
+using DesignPatterns.Strategy.Shared.Results;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DesignPatterns.Strategy.Features.Products;
+
+public class ProductRepositoryWithPostgresql(ApplicationDbContextPostgresql context) : IProductRepository
+{
+    private readonly ApplicationDbContextPostgresql _context = context;
+    private readonly DbSet<Product> _dbSet = context.Set<Product>();
+
+
+    public async Task<Result<List<Product>>> GetAllProductsAsync(CancellationToken cancellationToken = default)
+    {
+        var products = await _dbSet.ToListAsync(cancellationToken: cancellationToken);
+        if (products.IsNullOrEmpty())
+        {
+            products = [];
+        }
+
+        return Result.Success(products);
+    }
+
+    public async Task<Result<Product>> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var product = await _dbSet.FindAsync(id);
+        return product == null
+            ? Result.Failure<Product>(Error.NotFound("Product.NotFound", "Product couldn't found."))
+            : Result.Success(product);
+    }
+
+    public async Task<Result<Product>> SaveAsync(CreateProductDto createProduct, CancellationToken cancellationToken = default)
+    {
+        var product = new Product
+        {
+            Name = createProduct.Name,
+            Description = createProduct.Description,
+            Price = createProduct.Price
+        };
+
+        await _context.AddAsync(product, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success(product);
+    }
+
+    public async Task<Result> UpdateAsync(UpdateProductDto updateProduct, CancellationToken cancellationToken = default)
+    {
+        var getResult = await GetByIdAsync(updateProduct.Id, cancellationToken);
+        if (getResult.IsFailure)
+        {
+            return getResult;
+        }
+
+        var product = getResult.Data;
+
+        product.Name = updateProduct.Name;
+        product.Description = updateProduct.Description;
+        product.Price = updateProduct.Price;
+
+        _context.Update(product);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+
+    public async Task<Result> DeleteAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var getResult = await GetByIdAsync(id, cancellationToken);
+        if (getResult.IsFailure)
+        {
+            return getResult;
+        }
+
+        _context.Remove(getResult.Data);
+        await _context.SaveChangesAsync(cancellationToken);
+        
+        return Result.Success();
+    }
+}
---
+++ DesignPatterns.Strategy/Features/Products/ProductRepositoryWthMongoDb.cs
@@ -0,0 +1,77 @@
+﻿using DesignPatterns.Strategy.Shared.Results;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver;
+
+namespace DesignPatterns.Strategy.Features.Products;
+
+public class ProductRepositoryWthMongoDb : IProductRepository
+{
+    private readonly IMongoCollection<Product> _productsCollection;
+
+    public ProductRepositoryWthMongoDb(IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("MongoDB");
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase("DesignPatternsDB");
+        _productsCollection = database.GetCollection<Product>("Products");
+    }
+
+    public async Task<Result<List<Product>>> GetAllProductsAsync(CancellationToken cancellationToken = default)
+    {
+        var products =
+            await (await _productsCollection.FindAsync(_ => true, cancellationToken: cancellationToken)).ToListAsync(
+                cancellationToken: cancellationToken);
+
+        if (products.IsNullOrEmpty())
+        {
+            products = [];
+        }
+
+        return Result.Success(products);
+    }
+
+    public async Task<Result<Product>> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var product =
+            await (await _productsCollection.FindAsync(p => p.Id == id, cancellationToken: cancellationToken))
+            .FirstOrDefaultAsync(cancellationToken: cancellationToken);
+
+        return product == null
+            ? Result.Failure<Product>(Error.NotFound("Product.NotFound", "Product couldn't found."))
+            : Result.Success(product);
+    }
+
+    public async Task<Result<Product>> SaveAsync(CreateProductDto createProduct, CancellationToken cancellationToken = default)
+    {
+        var product = new Product
+        {
+            Name = createProduct.Name,
+            Description = createProduct.Description,
+            Price = createProduct.Price
+        };
+
+        await _productsCollection.InsertOneAsync(product, cancellationToken: cancellationToken);
+
+        return Result.Success(product);
+    }
+
+    public async Task<Result> UpdateAsync(UpdateProductDto updateProduct, CancellationToken cancellationToken = default)
+    {
+        await _productsCollection.FindOneAndReplaceAsync<Product>(p => p.Id == updateProduct.Id, new Product
+        {
+            Id = updateProduct.Id,
+            Name = updateProduct.Name,
+            Description = updateProduct.Description,
+            Price = updateProduct.Price
+        }, cancellationToken: cancellationToken);
+
+        return Result.Success();
+    }
+
+    public async Task<Result> DeleteAsync(int id, CancellationToken cancellationToken = default)
+    {
+        await _productsCollection.DeleteOneAsync(p => p.Id == id, cancellationToken);
+        return Result.Success();
+    }
+}
