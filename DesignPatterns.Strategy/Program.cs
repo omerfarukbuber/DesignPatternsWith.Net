55c039e46fac999cb0f4e58842e5742b8d341c61
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-11-20 23:30:09 +0300

    Register the ProductRepository by StrategyDesignPatternType value from the cookie

--- DesignPatterns.Strategy/Program.cs
+++ DesignPatterns.Strategy/Program.cs
@@ -1,6 +1,8 @@
 using Carter;
 using DesignPatterns.Strategy.Database;
 using DesignPatterns.Strategy.Database;
+using DesignPatterns.Strategy.Features.Products;
+using DesignPatterns.Strategy.Shared;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCarter();
 
 builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped<IProductRepository>(serviceProvider =>
+{
+    var httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
+    var strategyType = httpContextAccessor.HttpContext?.Request.Cookies["StrategyDesignPatternType"];
+    strategyType = string.IsNullOrWhiteSpace(strategyType) ? ((StrategyDesignPatternType)0).ToString() : strategyType;
+
+    return strategyType switch
+    {
+        nameof(StrategyDesignPatternType.SqlServer) => new ProductRepositoryWithSqlServer(
+            serviceProvider.GetRequiredService<ApplicationDbContextSqlServer>()),
+        nameof(StrategyDesignPatternType.Postgres) => new ProductRepositoryWithPostgresql(
+            serviceProvider.GetRequiredService<ApplicationDbContextPostgresql>()),
+        nameof(StrategyDesignPatternType.MongoDb) => new ProductRepositoryWthMongoDb(builder.Configuration),
+        _ => throw new ArgumentOutOfRangeException()
+    };
+});
 
 builder.Services.AddDbContext<ApplicationDbContextSqlServer>(options =>
 {
