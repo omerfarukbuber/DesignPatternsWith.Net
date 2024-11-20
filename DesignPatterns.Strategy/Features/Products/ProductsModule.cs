2e3dc747943713c42db4dca490080972a80bd126
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-11-20 22:20:08 +0300

    Add an endpoint to ProductsModule for change the database server on the runtime. This endpoint is able you to set cookie value

---
+++ DesignPatterns.Strategy/Features/Products/ProductsModule.cs
@@ -0,0 +1,37 @@
+﻿using Carter;
+using DesignPatterns.Strategy.Shared.Results;
+
+namespace DesignPatterns.Strategy.Features.Products;
+
+public class ProductsModule() : CarterModule("api/products")
+{
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("strategy/{strategyType:int}", (int strategyType, IHttpContextAccessor contextAccessor) =>
+        {
+            var options = new CookieOptions
+            {
+                Secure = true,
+                IsEssential = true,
+                HttpOnly = false
+            };
+            strategyType = strategyType < 0 ? 0 : strategyType;
+            strategyType = strategyType > 2 ? 0 : strategyType;
+
+            var strategyDesignPatternType = ((StrategyDesignPatternType)strategyType).ToString();
+
+            contextAccessor.HttpContext?.Response.Cookies.Append(
+                "StrategyDesignPatternType",
+                strategyDesignPatternType, options);
+
+            return Results.Ok(strategyDesignPatternType);
+        });
+    }
+}
+
+public enum StrategyDesignPatternType
+{
+    SqlServer = 0,
+    Postgres = 1,
+    MongoDb = 2
+}
