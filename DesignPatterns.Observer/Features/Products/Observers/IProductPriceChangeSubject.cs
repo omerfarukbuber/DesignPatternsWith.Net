4b8944cc50ea0cbb4082835ecc8d0ec6191afd42
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-11-22 19:09:27 +0300

    Add subject for notify observers

---
+++ DesignPatterns.Observer/Features/Products/Observers/IProductPriceChangeSubject.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPatterns.Observer.Features.Products.Observers;
+
+public interface IProductPriceChangeSubject
+{
+    void RegisterObserver(IProductPriceChangeObserver observer);
+    void UnregisterObserver(IProductPriceChangeObserver observer);
+    void NotifyObservers(Product product);
+}
---
+++ DesignPatterns.Observer/Features/Products/Observers/ProductPriceChangeSubject.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatterns.Observer.Features.Products.Observers;
+
+public class ProductPriceChangeSubject : IProductPriceChangeSubject
+{
+    private readonly List<IProductPriceChangeObserver> _observers = [];
+
+    public void RegisterObserver(IProductPriceChangeObserver observer)
+    {
+        _observers.Add(observer);
+    }
+
+    public void UnregisterObserver(IProductPriceChangeObserver observer)
+    {
+        _observers.Remove(observer);
+    }
+
+    public void NotifyObservers(Product product)
+    {
+        _observers.ForEach(o => o.ProductPriceChangedEvent(product));
+    }
+}
