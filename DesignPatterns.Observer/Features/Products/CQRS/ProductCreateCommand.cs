c048ce5a4d80e361e74c392cb1c81b315060d300
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-11-22 19:08:04 +0300

    Add create and update product functions with cqrs

---
+++ DesignPatterns.Observer/Features/Products/CQRS/ProductCreateCommand.cs
@@ -0,0 +1,9 @@
+﻿using DesignPatterns.Observer.Shared.Results;
+using MediatR;
+
+namespace DesignPatterns.Observer.Features.Products.CQRS;
+
+public sealed record ProductCreateCommand(
+    string Name,
+    string Description,
+    decimal Price) : IRequest<Result<Product>>;
