627d76973669b53d0c6db154b44a5e727b81a940
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-11-23 14:30:40 +0300

    Implement redis service and decorator services Decorator services implementing with using Scrutor library

--- DesignPatterns.Decorator/Program.cs
+++ DesignPatterns.Decorator/Program.cs
@@ -1,10 +1,19 @@
 using Carter;
 using DesignPatterns.Decorator.Database;
+using DesignPatterns.Decorator.Features.Products;
+using DesignPatterns.Decorator.Features.Products.Decorators;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
+
+builder.Services.AddStackExchangeRedisCache(setup =>
+{
+    var connection = builder.Configuration.GetConnectionString("Redis");
+    setup.Configuration = connection;
+});
 
 builder.Services.AddDbContext<ApplicationDbContextSqlServer>(options =>
 {
@@ -15,6 +24,18 @@ builder.Services.AddDbContext<ApplicationDbContextPostgresql>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"));
 });
+
+//Add Scrutor library --> 
+builder.Services.AddScoped<IProductRepository, ProductRepository>()
+    .Decorate<IProductRepository, ProductRepositoryCacheDecorator>();
+
+//builder.Services.AddScoped<IProductRepository>(provider =>
+//{
+//    var productRepository = new ProductRepository(provider.GetRequiredService<ApplicationDbContextSqlServer>());
+//    var productRepositoryCacheDecorator = new ProductRepositoryCacheDecorator(productRepository,
+//        provider.GetRequiredService<IDistributedCache>());
+//    return productRepositoryCacheDecorator;
+//});
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -29,5 +50,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapCarter();
 
 app.Run();
